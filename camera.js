7e201135045c6deaf5847d44f6b5acd1367197a8
Olivia Vann <xerovann860@gmail.com>
2026-07-13 20:34:42 +0100

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,5 +1,15 @@
+// HTML elements //
+
 const video = document.getElementById('preview');
 const startButton = document.getElementById('start-camera'); 
+const snapshot = document.getElementById('snapshot');
+const viewfinder = document.getElementById('viewfinder');
+const canvas = document.getElementById('captureCanvas');
+const captureButton = document.getElementById('capture-btn');
+const retakeButton = document.getElementById('retake-btn');
+
+
+// Turn on Camera //
 
 startButton.addEventListener('click', async () => {
   try {
@@ -20,3 +30,38 @@ startButton.addEventListener('click', async () => {
   }
   }
 });
+
+// Capture a Frame from video onto canvas //
+
+captureButton.addEventListener('click', () => {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+
+  const ctx = canvas.getContext('2d');
+
+  ctx.translate(canvas.width, 0);
+  ctx.scale(-1, 1);
+  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+  snapshot.src = canvas.toDataURL('image/png');
+  snapshot.hidden = false;
+  snapshot.hidden = true;
+
+  captureButton.hidden = true;
+  retakeButton.hidden = false;
+
+  viewfinder.classList.add('flash');
+  setTimeout(() => viewfinder.classList.remove('flash'), 400);
+});
+
+// Go back to live feed - RETAKE //
+
+retakeButton.addEventListener('click', () => {
+  snapshot.hidden = true;
+  video.hidden = false;
+
+  retakeButton.hidden = true;
+  captureButton.hidden = false;
+
+});
+
